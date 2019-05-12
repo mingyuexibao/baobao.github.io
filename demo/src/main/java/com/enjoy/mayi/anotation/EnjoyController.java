864b2dd26c213266b22b1aa70d58bb55bd9f45db
yangyueming <452230919@qq.com>
2019-05-12 17:42:18 +0800

    测试相关注解

---
+++ demo/src/main/java/com/enjoy/mayi/anotation/EnjoyController.java
@@ -0,0 +1,15 @@
+package com.enjoy.mayi.anotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)//作用范围
+@Retention(RetentionPolicy.RUNTIME)//系统运行时
+@Documented //javadoc
+public @interface EnjoyController {
+	String value() default "";
+
+}
---
+++ demo/src/main/java/com/enjoy/mayi/anotation/EnjoyRequestMapping.java
@@ -0,0 +1,16 @@
+package com.enjoy.mayi.anotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE,ElementType.METHOD})//作用范围
+@Retention(RetentionPolicy.RUNTIME)//系统运行时
+@Documented //javadoc
+public @interface EnjoyRequestMapping {
+
+	String value() default "";
+
+}
