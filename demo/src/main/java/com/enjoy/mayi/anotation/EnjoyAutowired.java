ae2be95a6a8fa6827d32c3357d527a56c6060bf0
yangyueming <452230919@qq.com>
2019-05-12 19:14:15 +0800

    测绘11

--- demo/src/main/java/com/enjoy/mayi/anotation/EnjoyAutowired.java
+++ demo/src/main/java/com/enjoy/mayi/anotation/EnjoyAutowired.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD22})//作用范围
+@Target({ElementType.FIELD})//作用范围
 @Retention(RetentionPolicy.RUNTIME)//系统运行时
 @Documented //javadoc
 public @interface EnjoyAutowired {
